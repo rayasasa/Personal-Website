76d635fd3039f8c32b63aad6c627612d017ddaed
rayasasa <sushma.rayasam@yahoo.com>
2017-12-28 16:25:33 -0500

    Added index.html to public

--- routes/stupid.js
+++ routes/stupid.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/stupid', function(req, res, next) {
+  res.render('stupid', { title: 'Express' });
 });
 
 module.exports = router;
