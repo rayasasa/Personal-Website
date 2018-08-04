23e551203a443990b83610feeaf4ae6f18aaa6df
Sushma <sushma@sushmas-air.lan>
2018-08-04 11:06:09 -0700

    .

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/page2', function(req, res){
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('')
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
